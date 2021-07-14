ea4fc4f24b53dd28ea61850ed99240649564bbef
unknown <vst87@outlook.com>
2021-07-14 20:29:44 +0530

    pwa react example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import Home from "./Home";
+import About from "./About";
+import Users from "./Users";
+import { Link, Route, BrowserRouter as Router, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar bg="primary" variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">ClickMe</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link>
+                <Link to="/">Home</Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/users">Users</Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/about">About</Link>
+              </Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+        <Switch>
+          <Route path="/users" component={Users}></Route>
+          <Route path="/about" component={About}></Route>
+          <Route path="/" component={Home}></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
